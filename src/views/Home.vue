0447558c1a6a41e54061ea1d7d8efd1c3daede9d
gnomerock <nuttakorn.buajarern@gmail.com>
2021-05-31 17:17:42 +0700

    install tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="copntainer">
+    <img alt="Entaneer Gear 42 logo" src="/logo.png" class="h-5">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
