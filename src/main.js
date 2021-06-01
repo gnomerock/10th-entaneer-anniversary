64883418753a0063624a4955c6868fb73e189826
gnomerock <nuttakorn.buajarern@gmail.com>
2021-06-01 19:40:42 +0700

    added font and firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/main.css'
+import firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: "AIzaSyALcy-dJ-mQNyvy9cy-SQba0sbKxErvIsw",
+  authDomain: "thyeargear42.firebaseapp.com",
+  projectId: "thyeargear42",
+  storageBucket: "thyeargear42.appspot.com",
+  messagingSenderId: "818161636537",
+  appId: "1:818161636537:web:fcacebbab5115198337768"
+};
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <div class="grid justify-items-center mt-1">
-      <img alt="Entaneer Gear 42 logo" src="/logo.png" class="rounded h-20">
+    <div class="flex items-center justify-center h-screen">
+      <div>
+        <img alt="Entaneer Gear 42 logo" src="/logo.png" class="rounded object-contain h-48 shadow-lg">
+        <button class="mt-2 rounded p-2 bg-blue-500 hover:bg-blue-400 text-white">ลงทะเบียนสั่งซื้อของที่ระลึก</button>
+      </div>
     </div>
   </div>
 </template>
